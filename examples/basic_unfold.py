1c6b43297894e743ba4542b0a2db695744423db5
mckib2 <nicholas.bgp@gmail.com>
2019-08-20 11:30:34 -0600

    Added animation to basic unfold example.

--- examples/basic_unfold.py
+++ examples/basic_unfold.py
@@ -1,6 +1,10 @@
 '''Basic demo of UNFOLD method.'''
 
+from time import time
+
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 from phantominator import dynamic
 
 from ktblast import unfold
@@ -18,4 +22,25 @@ if __name__ == '__main__':
     kspace_u[1::2, :, 1::2] = kspace[1::2, :, 1::2]
 
     # Run UNFOLD algorithm
+    t0 = time()
     recon = unfold(kspace_u)
+    print('Done with recon in %g sec' % (time() - t0))
+
+    # Some code to look at the animation
+    fig = plt.figure()
+    ax = plt.imshow(np.abs(recon[..., 0]), cmap='gray')
+
+    def init():
+        '''Initialize ax data.'''
+        ax.set_array(np.abs(recon[..., 0]))
+        return(ax,)
+
+    def animate(frame):
+        '''Update frame.'''
+        ax.set_array(np.abs(recon[..., frame]))
+        return(ax,)
+
+    anim = FuncAnimation(
+        fig, animate, init_func=init, frames=ph.shape[-1],
+        interval=40, blit=True)
+    plt.show()
